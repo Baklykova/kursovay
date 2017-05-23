06fe8b1ce9145c9d6ebd694f2f5bbe383795c031
Delaila <valjushka95@bk.ru>
2017-05-23 19:45:21 +0300

    функции заработали!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ messages/for-translation/Journal.php
@@ -0,0 +1,6 @@
+<?php
+if(false){
+    echo Yii::t('app', 'AppJournalFull');
+    echo Yii::t('app', 'AppJournalView');
+    echo Yii::t('app', 'AppJournalEdit');
+}
---
+++ messages/ru/app.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Маша
+ * Date: 14.12.2015
+ * Time: 1:28
+ */
+
+return [
+    /*Description Forms*/
+    'Sign out' => 'Выход',
+    'Sign in' => 'Войти',
+    'Register' => 'Регистрация',
+
+    'New' => 'Новый',
+    'Password' => 'Пароль',
+    'Create' => 'Создание',
+    'Create new' => 'Создать новую запись',
+    'Update' => 'Обновить запись',
+    'Delete' => 'Удалить',
+    'Delete All' => 'Удалить записи',
+    'success' => 'успешно заверешно',
+    'Details' => 'Подробнее',
+    'Record # ' => 'Запись № ',
+
+    'Active' => 'Активность',
+    'Description' => 'Описание',
+    'Desc Other' => 'Примечание',
+    'Relations' => 'Связанные данные',
+    'Date' => 'Дата',
+    'Name' => 'Название',
+    'Full Name' => 'Полное наименование',
+    'Country' => 'Страна',
+    'Address' => 'Адрес',
+    'Advance Search' => 'Расширенный поиск',
+
+    'Edit' => 'Редактировать',
+    'Save' => 'Сохранить',
+    'Copy' => 'Копировать',
+
+    'Close' => 'Закрыть',
+    'Cancel' => 'Закрыть',
+    'List' => 'Список',
+    'Select' => 'Выберите значение',
+    'Error Download' => 'Ошибка загрузки данных',
+    'listing' => '. Список',
+    'View' => 'Просмотр',
+    'Full list' => 'Полный список',
+    'Update {modelClass} #{id}' => 'Изменение {modelClass} #{id}',
+    'Choose' => 'Выберите значение',
+
+    //Users messages
+    'Are you sure?' => 'Вы уверены?',
+    'Are you sure want to delete this item' => 'Вы уверены, что хотите удалить этот элемент?',
+    'Resize table columns just like a spreadsheet by dragging the column edges.' =>
+        'Изменение размера столбцов таблицы происходит так же, как таблицы - перетаскивая края столбцов.',
+    '* Resize table columns just like a spreadsheet by dragging the column edges.' =>
+        '* Изменение размера столбцов таблицы происходит так же, как таблицы - перетаскивая края столбцов.',
+
+    'With selected' => 'действия с выделенными записями ',
+    /*/Description Forms*/
+
+    'Search' => 'Поиск',
+    'Reset' => 'Сброс',
+    'Reset Grid' => 'Обновить таблицу',
+
+    'User Roles' => 'Роли пользователей',
+    'UserRole' => 'Роль пользователя',
+    'Userinfos' => 'Информация о пользователях',
+    'Userinfo' => 'Пользователь',
+    'User' => 'Консультант',
+
+    'You must select item(s) to use this action' => 'Нет выбранных значений',
+    'No selection' => 'Ничего не выбранно',
+    'Trans Type Stations listing' => 'Типы станций. Список',
+    'Create new TransTypeStation' => 'Добавить новый тип станции',
+
+    'Create new {modelClass}' => 'Создание нового параметра: {modelClass}',
+    'Create {modelClass}' => 'Создание: {modelClass}',
+    'Incorrect username or password.' => 'Неверное имя пользователя или пароль.',
+
+
+    'Value' => 'Значение',
+
+    /**Frontend*/
+    'Submit' => 'Отправить',
+    'Verification Code' => 'Проверочный код',
+    'Subject' => 'Тема сообщения',
+    'Body' => 'Текст сообщения',
+    'Contacts' => 'Контакты',
+    'Phone' => 'Телефон',
+    'My phone number is: ' => 'Мой контактный номер телефона: ',
+    'Thank you for contacting us. We will respond to you as soon as possible.' =>
+        'Благодарим Вас за обращение к нам. Мы ответим вам в ближайшее время.',
+    'There was an error sending email.' => 'При отправке сообщения возникли ошибки, повторите запрос позже.',
+    'New password was saved.' => 'Новый пароль успешно сохранен.',
+    'success' => 'Успешное действие',
+    '#pp' => '№№ п/п',
+    'Count people' => 'Количество граждан',
+    'Reports' => 'Отчеты',
+
+
+    /** Справочники */
+    'Cons Points' => 'Справочник пунктов приема',
+    'ConsPoint' => 'Пункт приема',
+    'Cons Point' => 'Пункт приема',
+    'Cons point' => 'Пункт приема',
+    'Cons points' => 'Пункт приема',
+    'Name cons point' => 'Название пункта приема граждан',
+
+    'Sp Draft Docs' => 'Справочник типов составленных документов',
+    'SpDraftDoc' => 'Тип составленного документа',
+    'SpDraftDocs' => 'Составленные документы',
+    'Sp Draft Doc' => 'Тип составленного документа',
+    'Sp draft doc' => 'Тип составленного документа',
+    'Sp draft docs' => 'Тип составленного документа',
+    'Name sp draft doc' => 'Название типа составленного документов',
+
+    'Sp Type Cons' => 'Справочник типов консультаций',
+    'SpTypeCon' => 'Тип консультаций',
+    'Sp Type Con' => 'Тип консультаций',
+    'Sp type con' => 'Тип консультаций',
+    'Sp type cons' => 'Тип консультаций',
+    'Name sp type con' => 'Название типа консультаций',
+
+    'Sprav Kategs' => 'Справочник категорий граждан',
+    'SpravKateg' => 'Категория граждан',
+    'SpravKategs' => 'Категории граждан',
+    'Sprav Kateg' => 'Категория граждан',
+    'Sprav kateg' => 'Категория граждан',
+    'Sprav kategs' => 'Категория граждан',
+    'Name sprav kateg' => 'Название категории граждан',
+
+    'Sp Categ Pravo Voprs' => 'Справочник категорий правовых вопросов',
+    'SpCategPravoVopr' => 'Справочник категорий правовых вопросов',
+    'Sp Categ Pravo Vopr' => 'Категории вопросов',
+    'SpCategPravoVoprs' => 'Категории правовых вопросов',
+    'Sp categ pravo vopr' => 'Справочник категорий правовых вопросов',
+    'Sp categ pravo voprs' => 'Справочник категорий правовых вопросов',
+    'Name sp categ pravo voprs' => 'Справочник категорий правовых вопросов',
+
+    /**Журнал*/
+    'F' => 'Фамилия',
+    'I' => 'Имя',
+    'O' => 'Отчество',
+    'Reg Num' => 'Рег.номер',
+    'Cons Points ID' => 'Пункт приема',
+    'User ID' => 'Консультант',
+    'Sp Type Cons ID' => 'Тип консультации',
+    'Sprav Kateg ID' => 'Категория гражданина',
+    'Sp Draft Docs ID' => 'Составленные документы',
+    'Sp Categ Pravo Vopr ID' => 'Категории правовых вопросов',
+    'Deleted' => 'Пометка на удаление',
+    'Journals' => 'Журнал учета консультаций',
+    'Journal' => 'Журнал учета консультаций.',
+    'Create Journal' => 'Создать новую запись журнала',
+
+    'UserHasConsPoint' => 'Привязка консультантов и пунктов приема',
+    'User Has Cons Points' => 'Привязка консультантов к пунктам приема',
+];
